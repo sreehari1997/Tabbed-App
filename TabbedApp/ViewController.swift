145ac8164135483949ba3b903058f1d287216b56
SREEHARI K.V <sreeharivijayan619@gmail.com>
2018-06-20 15:45:44 +0530

    Initial Commit

---
+++ TabbedApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabbedApp
+//
+//  Created by SREEHARI K.V on 20/06/18.
+//  Copyright © 2018 SREEHARI K.V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
