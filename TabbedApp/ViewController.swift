d43cb286fa6916e4656814612d0b5f61b097dfe2
SREEHARI K.V <sreeharivijayan619@gmail.com>
2018-06-20 16:27:24 +0530

    imagepositioned

--- TabbedApp/ViewController.swift
+++ TabbedApp/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //entry point
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_first"))
+        view.addSubview(imageView)
+        //imageView.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        //imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
     }
 
     override func didReceiveMemoryWarning() {
